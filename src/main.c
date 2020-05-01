5f93bbc85aa34320b80e5c6dba4082ece6d73127
Anton Muravyov <tsb99x@gmail.com>
2020-05-01 17:53:48 +0300

    Use SEH wrapper in main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <signal.h>
 
+#ifdef WIN32
+#include <excpt.h>
+#endif
+
 #include "utility.h"
 #include "routes/_routes.h"
 
@@ -68,10 +72,22 @@ struct scgi_ctx req_ctx = {
 int main(
         void
 ) {
+        #ifdef WIN32
+        __try {
+        #endif
+
         IPRINTF("Starting Tusk Server");
         listener = init_listener(BACKLOG_SIZE);
         IPRINTF("Server launch success");
         signal(SIGINT, shutdown_handler);
         accept_connections(listener, process_scgi_message, (struct sock_ctx *) &req_ctx);
+
+        #ifdef WIN32
+        } __except(EXCEPTION_EXECUTE_HANDLER) {
+                EPRINTF("Exception occurred, code: %d", GetExceptionCode());
+                return EXIT_FAILURE;
+        }
+        #endif
+
         return EXIT_SUCCESS;
 }
